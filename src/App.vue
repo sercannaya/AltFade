8009bc48ee61def4a8875144075691573467cfa5
Sercan Naya <sercannayaa@gmail.com>
2026-04-19 11:39:17 +0300

    refactor: remove unnecessary import

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
+import { ref, onMounted } from "vue";
 import { invoke } from "@tauri-apps/api/core";
 import { useI18n } from "vue-i18n";
 
